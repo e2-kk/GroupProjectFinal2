653fd2fd481a04eaf5664a0fdbdbcbf6fab4c7e0
Maria-source-cell <manuelabirnat@gmail.com>
2025-08-07 20:18:31 +0200

    Refactor cactus obstacle logic and update styles
    Replaces the single moving cactus with dynamic obstacle generation and movement in script.js. Updates styles.css to support multiple obstacles and adjusts positioning. Also updates the cacti.png asset.

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-let cactus = document.getElementById("cactus")
-let cactusPosition = 600;
+const cactusContainer = document.getElementById("cactus")
+let lastCactusTime = 0;
+const cactusInterval = 5000;
 
-function moveCactus() {
+/* function moveCactus() {
     cactusPosition -= 5;
     if (cactusPosition < - 20) cactusPosition = 600;
     cactus.style.left = cactusPosition + "px";
 }
 
-setInterval(moveCactus, 50)
+setInterval(moveCactus, 50) */
 
+function createObstacle() {
+    const obstacle = document.createElement("div");
+    obstacle.classList.add("cactus-obstacles");
+
+    //Random height
+    const height = Math.floor(Math.random() * 30) + 50;
+    obstacle.style.height = height + "px";
+    obstacle.style.left = "100%";
+
+    cactusContainer.appendChild(obstacle);
+    moveObstacle(obstacle);
+}
+
+function moveObstacle(obstacle) {
+    let position = window.innerWidth;
+
+    const interval = setInterval(() => {
+        position -= 5;
+        obstacle.style.left = position + "px";
+
+        // Remove when off screen
+        if (position < -60) {
+            obstacle.remove();
+            clearInterval(interval)
+        }
+
+
+    }, 50);
+}
+
+function startObstacleLoop() {
+    setInterval(() => {
+        const now = Date.now();
+        const timeSinceLast = now - lastCactusTime;
+
+        if(Math.random() < 0.5 && timeSinceLast > cactusInterval) {
+            createObstacle();
+            lastCactusTime = now
+        }
+    }, 500); 
+}
+
+startObstacleLoop();
